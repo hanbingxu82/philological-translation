1fcc498bae1b3285704b04144acead1474bf2f5c
韩炳旭 <905583741@qq.com>
2024-01-15 17:58:40 +0800

    feat:调整 ctrl+option+m 驼峰键入输入，并且调整了因为内容中有注释导致的移除问题

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
 # philological-translation
 
-<!-- [![Marketplace Version](https://vsmarketplacebadge.apphb.com/version-short/tinkerbell-hx.philological-translation.svg)](https://marketplace.visualstudio.com/items?itemName=tinkerbell-hx.philological-translation)
-[![Installs](https://vsmarketplacebadge.apphb.com/installs/tinkerbell-hx.philological-translation.svg)](https://marketplace.visualstudio.com/items?itemName=tinkerbell-hx.philological-translation) -->
-
 ## 功能特性
 
 philological-translation 是一款专为注解和翻译设计的VSCode插件。它允许用户对选中的代码片段进行注释并调用翻译服务进行翻译。
@@ -11,11 +8,43 @@ philological-translation 是一款专为注解和翻译设计的VSCode插件。�
 
 - **Comment and Translate Selection**：选中代码后，通过命令面板或快捷键执行此操作，可以快速将选中文本添加注释并进行翻译。
 
+### 获取百度翻译API的App ID和API Key
+
+#### 1. 注册百度开发者账号
+
+要使用百度翻译API，首先需要在百度开放平台注册一个开发者账号。访问 [百度AI开放平台](https://ai.baidu.com/) 并完成以下步骤：
+
+- 点击顶部导航栏中的“立即注册”，按照提示填写邮箱、手机号码等信息进行注册。
+- 完成邮箱验证或手机短信验证。
+
+#### 2. 创建应用并获取App ID和API Key
+
+注册成功后，您需要创建一个新的应用来获得API密钥：
+
+- 登录到您的百度开发者中心。
+- 导航至相关服务管理页面，例如：【我的应用】或直接访问[百度翻译开放平台](https://fanyi-api.baidu.com/）。
+- 点击【创建应用】按钮，填写应用名称、描述以及选择应用类型（如Web应用）。
+- 提交后，系统会自动分配一个唯一的App ID给你的应用。
+- 在应用详情页面中，找到API Key（可能被称为“密钥”、“秘钥”或“Access Key”），请妥善保存这个密钥，因为后续调用API时需要用到。
+
+### 注意事项：
+
+- App ID和API Key是敏感信息，请勿公开分享或泄露。
+- 百度翻译API可能有免费试用额度与付费使用模式，请根据需求合理规划使用。
+- 使用API前，请确保已阅读并遵守百度翻译API的服务条款和使用指南。
+
 ### 配置选项
 
 在VSCode设置中可自定义以下参数：
 - **App ID** (`philologicalTranslation.appId`)：用于与翻译服务通信的应用ID，请从服务提供商处获取。
 - **API Key** (`philologicalTranslation.apiKey`)：用于验证身份的API密钥。请注意保持安全，不要公开分享。
+
+```
+  {
+    "philologicalTranslation.appId": "百度翻译appId",
+    "philologicalTranslation.apiKey": "百度翻译apiKey"
+  }
+```
 
 ## 安装方法
 
--- extension.js
+++ extension.js
@@ -11,7 +11,7 @@ const TranslationService = require('./translate-service')
  */
 // 激活插件时注册事件处理器
 function activate (context) {
-	// 注册自定义注释和翻译命令
+	// 注册自定义注释和翻译命令 ctrl+m
 	let customCommentDisposable = vscode.commands.registerCommand('custom.action.jsCommentAndTranslate', async () => {
 		const editor = vscode.window.activeTextEditor;
 		if (!editor) return;
@@ -21,28 +21,29 @@ function activate (context) {
 		}
 	});
 
-	// 注册一个命令用于提示用户如何添加快捷键
-	let showKeybindingInstructionDisposable = vscode.commands.registerCommand('custom.showKeybindingInstruction', () => {
-		vscode.window.showInformationMessage(`若要为"Comment and Translate"功能添加快捷键，请在'keybindings.json'文件中加入以下内容：
-			
-			{
-					"key": "ctrl+m",
-					"command": "custom.action.jsCommentAndTranslate",
-					"when": "editorTextFocus && editorLangId == javascript"
-			}`);
+	// 注册自定义注释和翻译命令驼峰式 ctrl+alt+m
+	let customCommentDisposableHump = vscode.commands.registerCommand('custom.action.jsCommentAndTranslateHump', async () => {
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) return;
+
+		for (const selection of editor.selections) {
+			await handleCommenting(editor, selection, true);
+		}
 	});
 
 	// 添加命令到上下文菜单（可选）
 	context.subscriptions.push(vscode.commands.registerCommand('editorContext.customCommentAndTranslate', () => {
 		vscode.commands.executeCommand('custom.action.jsCommentAndTranslate');
+		vscode.commands.executeCommand('custom.action.jsCommentAndTranslateHump');
 	}));
+
 
 	// 注册命令至命令面板以便用户执行
 	context.subscriptions.push(customCommentDisposable);
-	context.subscriptions.push(showKeybindingInstructionDisposable);
+	context.subscriptions.push(customCommentDisposableHump);
 
 	// 提示用户运行 'custom.showKeybindingInstruction' 命令以了解如何添加快捷键
-	vscode.window.showInformationMessage("欢迎使用 Comment and Translate 扩展！要添加快捷键，请运行 'Custom: Show Keybinding Instruction' 命令。");
+	vscode.window.showInformationMessage("欢迎使用 天才翻译 扩展！请在 setting 中自行添加 百度翻译（appId、apiKey）后，即可执行'Custom: Show Keybinding Instruction' 命令 || Ctrl+M 进行翻译啦！");
 }
 
 // 假设你已经有了从百度翻译获取的appId和apiKey
@@ -52,14 +53,17 @@ const apiKey = extensionConfig.get('apiKey');
 
 const TranslationServiceInstance = new TranslationService(appId, apiKey);
 
-async function translateText (chineseText, form, to, editor, line) {
+async function translateText (chineseText, form, to, editor, line, hump) {
 	try {
-		const translatedText = await TranslationServiceInstance.translateComment(chineseText, form, to);
+		let translatedText = await TranslationServiceInstance.translateComment(chineseText, form, to);
+		// 如果是转驼峰方法的话就执行一下 转驼峰函数
+		if (hump && form === 'zh') {
+			translatedText = translateAndConvertToCamelCase(translatedText)
+		}
 		// 替换选中的注释文本为翻译结果
 		// 将翻译结果插入到注释内容之后（这里假设是同一行）
 		const translationInsertionPosition = new vscode.Position(line, Number.MAX_VALUE);
-		const translationWithNewLine = `  天才翻译：${translatedText}`;
-		console.log(translationWithNewLine, 12345)
+		const translationWithNewLine = `  天才翻译 : ${translatedText}`;
 		await editor.edit(editBuilder => {
 			editBuilder.insert(translationInsertionPosition, translationWithNewLine);
 		});
@@ -68,6 +72,18 @@ async function translateText (chineseText, form, to, editor, line) {
 	} catch (error) {
 		console.error('Error during translation:', error.message);
 	}
+}
+
+function translateAndConvertToCamelCase (text) {
+	// 首先移除所有空格并将字符串分割成单词数组
+	const words = text.replace(/\s+/g, ' ').trim().split(' ');
+
+	// 将单词数组转换为驼峰格式（小驼峰）
+	const camelCaseText = words.reduce((acc, word, index) => {
+		return acc + (index === 0 ? word.toLowerCase() : word.charAt(0).toUpperCase() + word.slice(1).toLowerCase());
+	}, '');
+
+	return camelCaseText;
 }
 
 function detectLanguageInComment (comment) {
@@ -83,7 +99,7 @@ function detectLanguageInComment (comment) {
 		return '';
 	}
 }
-async function customCommentForJS (editor, selection) {
+async function customCommentForJS (editor, selection, hump) {
 	// 对每个选区进行单独的注释操作
 	// 对每个选区内的每一行分别执行注释操作
 	for (let line = selection.start.line; line < selection.end.line + 1; line++) {
@@ -93,7 +109,7 @@ async function customCommentForJS (editor, selection) {
 		let commentedLineText = editor.document.lineAt(line).text.trim().substring(3); // 去掉'// '
 
 		// 判断是否包含注释，如果包含就取消注释   并且不翻译
-		if (currentLineText.includes('//')) {
+		if (currentLineText.includes('//') && currentLineText.indexOf('//') === 0) {
 			const rangeToUpdate = new vscode.Range(new vscode.Position(line, 0), new vscode.Position(line, Number.MAX_VALUE));
 			await editor.edit(editBuilder => {
 				editBuilder.replace(rangeToUpdate, commentedLineText);
@@ -118,16 +134,16 @@ async function customCommentForJS (editor, selection) {
 			continue
 		}
 
-		await translateText(commentedLineText, isFrom, isTo, editor, line)
+		await translateText(commentedLineText, isFrom, isTo, editor, line, hump)
 		// 在这里执行你的自定义逻辑，例如：
 		// vscode.window.showInformationMessage('You just commented/uncommented a line!');
 	}
 
 }
 // 处理注释时触发的动作
-async function handleCommenting (editor, selection) {
+async function handleCommenting (editor, selection, hump = false) {
 	// 对选区进行注释操作（可以调用VSCode内置API或自定义逻辑）
-	await customCommentForJS(editor, selection);
+	await customCommentForJS(editor, selection, hump);
 }
 
 // 插件停用时清理资源
